3c6286da1f25cb5e241771b7e24c7abe413b4b0c
suhxail <hmdsuhail99@gmail.com>
2023-05-23 14:33:09 +0530

    demo

---
+++ day-14/script.js
@@ -0,0 +1,45 @@
+// window.localStorage.setItem('username', 'suhail')
+
+// console.log(window.localStorage.getItem('username'));
+
+// window.localStorage.setItem('isloggedIn', true);
+
+// console.log(window.localStorage);
+
+// setTimeout
+
+// window.setTimeout(() => console.log('after two seconds'), 5000);
+
+// let seconds = 10;
+
+// let timer = setInterval(() => {
+//     console.log(seconds);
+//     seconds--;
+// }, 1000);
+
+// setTimeout(() => {
+//     clearInterval(timer);
+//     setTimeout(() => {
+//         console.log('Happy New Year');  
+//     }, 1000)
+//     // console.log('Happy New Year');
+// }, 10000);
+
+// let name = prompt('your name?')
+// console.log(name);
+
+// alert('hi')4
+
+//  let confirmAnswer =confirm('are you sure');
+
+//  if(confirmAnswer) {
+//     console.log('clicked ok');
+//  } else {
+//     console.log('clicked cancel');
+//  }
+
+// console.log(window.innerWidth,window.innerHeight);
+
+// console.log(window.document);
+
+console.log(window.open('https://www.google.com','_blank'))
